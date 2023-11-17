d705f52f97416277de69f436b1878ca3ed0e2472
minhaj-boop <imminhajj@gmail.com>
2023-11-17 14:59:43 +0600

    updated schema and database

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -2,7 +2,19 @@ import Card from "../card/Card";
 import Pagination from "../pagination/Pagination";
 import styles from "./cardList.module.css";
 
-const CardList = () => {
+const getData = async (page) => {
+  const res = await fetch(`http://localhost:3000/api/posts?page=${page}`, {
+    cache: "no-store",
+  });
+  if (!res.ok) {
+    throw new Error("Failed");
+  }
+  return res.json();
+};
+
+const CardList = async ({ page }) => {
+  const data = await getData();
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Recent Post</h1>
